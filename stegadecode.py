c6ea985db74a918065e916b02c2bcacedb38f02e
canta2899 <56071114+canta2899@users.noreply.github.com>
2021-05-05 04:20:16 +0200

    Cleaning code and adding some comments

--- stegadecode.py
+++ stegadecode.py
@@ -2,33 +2,53 @@ from PIL import Image
 from bitarray import bitarray
 import sys
 
-IMAGE = sys.argv[1]
-SPLIT = '%%%'
-if len(sys.argv) > 2:
-    SPLIT = sys.argv[2]
-    print('Changed split to %s' % SPLIT)
+"""
+    Extracts all the bits embedded inside an image file
+    produced using LSB Image Steganography techniques in
+    order to retrieve the hidden regular file.
 
+    Usage: python3 [scriptname] [/path/to/image]
 
-image = Image.open(IMAGE)
-width, height = image.size
-pxmatrix = image.load()
+    The image must have lossless encoding, otherwise
+    information might be compromised or lost during 
+    compression.
 
-def extract_lsb(pixel):
-    return pixel & 1
+"""
 
-def decode():
-    b = bitarray()
+def decode(image):
+
+    # Pixel Matrix
+    width, height = image.size
+    pxmatrix = image.load()
+
+    # Will store all the LSB 
+    b = bitarray() 
+
+    # Retrieving LSB for every RGB pixel tuple
     for py in range(0, height):
         for px in range(0, width):
             red, green, blue = pxmatrix[px, py]
-            b.append(extract_lsb(red))
-            b.append(extract_lsb(green))
-            b.append(extract_lsb(blue))
+            b.append(red & 1)
+            b.append(green & 1)
+            b.append(blue & 1)
 
     content = b.tobytes().split(b'split')
+
+    # raw bytes content and file extension
     data = content[0]
     ext = content[1].decode('utf-8')
+
+    # Saving as bytes to a file 
     with open(f'out{ext}', 'wb') as a:
         a.write(content[0])
 
-decode()
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print("Provide a valid path to an image file")
+        sys.exit(1)
+
+    IMAGE = sys.argv[1]
+    image = Image.open(IMAGE)
+    decode(image)
--- stegaencode.py
+++ stegaencode.py
@@ -3,58 +3,103 @@ from bitarray import bitarray
 import sys
 import os
 
+"""
+    Given an image file and a path to a regular file, produces a 
+    new image file which embeds the raw content of the reg file
+    by using LSB Image Steganography techniques.
+
+    Usage: python3 [scriptname] [/path/to/image] [/path/to/file]
+
+    The image produced as output will be saved as PNG so that the
+    content won't be compromised because of lossy compression.
+"""
+
+# Builds a bit array containing both raw content and ext
+# given a path to a regular file
 def build_message(msg_file):
+    
+    # Reads file
     with open(msg_file, "rb") as file:
         msg = file.read()
+
+    # Reads extension
     ext = os.path.splitext(msg_file)[-1].lower()
+
+    # Builds the whole message
     msg = msg + b'split' + bytes(ext, 'utf-8') + b'split'
+
+    # Conversion to bit array
     bits = bitarray()
     bits.frombytes(msg)
     bits = [int(bit) for bit in bits]
+
     return bits
 
-def new_value(pixel, bit):
-    return (pixel & ~1) | bit
 
-def update_color(pixel):
-    global index, bound
+# Modifies the LSB for every color of every pixel
+def new_value(pixelcolor, bit):
+    return (pixelcolor & ~1) | bit
+
+
+# Updates pixel LSB if conditions are verified
+def update_color(pixel, index, bound, bits_msg):
     if index < bound:
-        newpixel = new_value(pixel, bits_msg[index])
-        index += 1
-        return newpixel
+        return (pixel & ~1) | bits_msg[index]
     return pixel
 
-def embed():
+
+# Checks if the picture will be able to contain the whole file
+def check_dimension(bound, width, height):
+    if bound > width*height*3:
+        print("This picture can't cointain the given content")
+        exit(1)
+
+
+# Builds a new image that embeds the file content
+def embed(image, msg_file):
+    width, height = image.size
+    pxmatrix = image.load()
+   
+    # Builds bit array from message and checks if it can be
+    # stored inside the picture
+    bits_msg = build_message(msg_file)
+    bound = len(bits_msg)
+    check_dimension(bound, width, height)
+        
+    # The index will keep track of the next bit to be stored
+    index = 0
+    
+    # For color or every pixel, update the LSB accordingly
     for py in range(0, height):
         for px in range(0, width):
             if index >= bound:
                 break
 
-            red, green, blue = pxmatrix[px, py]
+            rgb = pxmatrix[px, py]
+            newcolors = []
 
-            red = update_color(red)
-            green = update_color(green)
-            blue = update_color(blue)
+            for color in rgb:
+                newcolors.append(update_color(color, index, bound, bits_msg))
+                index += 1
 
-            pxmatrix[px, py] = (red, green, blue)
+            pxmatrix[px, py] = tuple(newcolors)
 
     print('Done. Saving...')
-    image.save(f'embedded.png')
+    image.save(f'embedded.png') # might take some seconds with big images
 
 
-IMAGE = sys.argv[1]
-MESSAGE_FILE = sys.argv[2]
+if __name__ == '__main__':
+    
+    if len(sys.argv) < 3:
+        print("Not enough arugments. Usage:")
+        print("python3 [name] [path/to/picture] [path/to/file]")
+        sys.exit(1)
 
-image = Image.open(IMAGE)
-width, height = image.size
-pxmatrix = image.load()
+    IMAGE = sys.argv[1]
+    MESSAGE_FILE = sys.argv[2]
 
-bits_msg = build_message(MESSAGE_FILE)
-bound = len(bits_msg)
-index = 0
+    image = Image.open(IMAGE)
 
-if bound > width*height*3:
-    print("This picture can't cointain the given content")
-    exit(1)
+    embed(image, MESSAGE_FILE)
 
-embed()
+
